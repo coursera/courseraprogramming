5f9aa5416859e27701614db9b1fe4cb62ad32745
Brennan Saeta <saeta@coursera.org>
2015-08-06 16:26:38 -0700

    Release version 0.5.0
    Release notes:
     - The brand new upload subcommand
     - A new OAuth2 subsystem for communicating with Coursera
     - A number of minor bugfixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='courseraprogramming',
-      version='0.2.1',
+      version='0.5.0',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
