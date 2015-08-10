1fef975e0a477e8d345bba4f5e2f3f88a8242018
Brennan Saeta <saeta@coursera.org>
2015-08-10 15:28:28 -0700

    Release version 0.5.1
    This release is a minor bugfix release making the OAuth2 library more
    robust to more intermediate states on the file system / auth cache.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='courseraprogramming',
-      version='0.5.0',
+      version='0.5.1',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
