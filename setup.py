937b3ba62905b326420ce9209f7ca986435daa5a
Brennan Saeta <saeta@coursera.org>
2015-08-27 21:03:14 -0700

    Release version 0.6.0
    Major change: support new grade output formats.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='courseraprogramming',
-      version='0.5.1',
+      version='0.6.0',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
