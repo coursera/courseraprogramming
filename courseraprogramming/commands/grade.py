ebf70f936b3519a0a1a588b6a39c0f39f1de533a
Brennan Saeta <saeta@coursera.org>
2015-12-07 11:18:31 -0800

    Support multi-line output strings when parsing JSON.

--- courseraprogramming/commands/grade.py
+++ courseraprogramming/commands/grade.py
@@ -55,11 +55,7 @@ def run_container(docker, container, args):
     stdout_output = docker.logs(container, stdout=True, stderr=False)
     error_in_grader_output = False
     try:
-        lines = stdout_output.split('\n')
-        # Assert there is only one non-empty line in the output.
-        if len([line for line in lines if line != '']) != 1:
-            logging.error("The output did not have the right number of lines.")
-        parsed_output = json.loads(lines[0])
+        parsed_output = json.loads(stdout_output)
     except ValueError:
         logging.error("The output was not a valid JSON document.")
         error_in_grader_output = True
