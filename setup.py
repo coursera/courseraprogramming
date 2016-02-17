3c911bd701eb198da1820d3a365f587b4e0fd6cc
Brennan Saeta <saeta@coursera.org>
2016-02-16 17:25:34 -0800

    Publish 0.9.0 of `courseraprogramming`.
    Key change:
     - Version check for docker. (Requires >=1.5.0 and <= 1.9.1)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='courseraprogramming',
-      version='0.8.0',
+      version='0.9.0',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
