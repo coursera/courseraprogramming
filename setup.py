5d1058967f7bd4f711f0a50100fafb48553dc9ca
Brennan Saeta <saeta@coursera.org>
2016-03-31 12:08:50 -0700

    Release 0.12.
     - New Feature: Add resource customization. When uploaded, a user can specify a custom amount of resources (CPU, Memory, Timeout) available to the grader.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='courseraprogramming',
-      version='0.11.0',
+      version='0.12.0',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
