962295a6ea7762860e4cc941be64c6bf43b4ccbc
Brennan Saeta <saeta@coursera.org>
2016-04-02 11:14:07 -0700

    Release 0.12.1 (bugfix for upload)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='courseraprogramming',
-      version='0.12.0',
+      version='0.12.1',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
