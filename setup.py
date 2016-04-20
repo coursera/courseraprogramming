cde9c30cc12f98360395baac31b348a105421432
Brennan Saeta <saeta@coursera.org>
2016-04-19 19:58:31 -0700

    Release version 0.15.0
     - Support configurable memory limits during grade local
     - Remove docker <=1.9.1 restriction.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='courseraprogramming',
-      version='0.14.0',
+      version='0.15.0',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
